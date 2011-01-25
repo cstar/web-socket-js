942cf688b79469aa2be329a1114bdb8035e4df65
Eric Cestari <ecestari@mac.com>
2011-01-25 13:41:37 +0100

    NoTLS version of the flash file.
    It's 17Kb instead of 180Kb.

--- flash-src/build.sh
+++ flash-src/build.sh
@@ -2,9 +2,10 @@
 
 # You need Flex 4 SDK:
 # http://opensource.adobe.com/wiki/display/flexsdk/Download+Flex+4
-
-mxmlc -static-link-runtime-shared-libraries -output=../WebSocketMain.swf WebSocketMain.as &&
-mxmlc -static-link-runtime-shared-libraries -output=../WebSocketMainInsecure.swf WebSocketMainInsecure.as &&
+BIN=/Users/cstar/Downloads/flex_sdk_4.1.0.16076_mpl/bin
+$BIN/mxmlc -static-link-runtime-shared-libraries -output=../WebSocketMain.swf WebSocketMain.as &&
+$BIN/mxmlc -static-link-runtime-shared-libraries -output=../WebSocketMainInsecure.swf WebSocketMainInsecure.as &&
+$BIN/mxmlc -static-link-runtime-shared-libraries -output=../WebSocketMainNoTLS.swf WebSocketMainNoTLS.as &&
 cd .. &&
 zip WebSocketMainInsecure.zip WebSocketMainInsecure.swf &&
 rm WebSocketMainInsecure.swf
