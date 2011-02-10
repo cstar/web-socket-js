8de6b485999e73406065c96513f4ba3b9e14ad78
Eric Cestari <ecestari@mac.com>
2011-02-10 16:50:28 +0100

    More SWF builds: - Insecure no TLS
    Updated build file

--- flash-src/build.sh
+++ flash-src/build.sh
@@ -5,6 +5,7 @@
 BIN=/Users/cstar/Downloads/flex_sdk_4.1.0.16076_mpl/bin
 $BIN/mxmlc -static-link-runtime-shared-libraries -output=../WebSocketMain.swf WebSocketMain.as &&
 $BIN/mxmlc -static-link-runtime-shared-libraries -output=../WebSocketMainInsecure.swf WebSocketMainInsecure.as &&
+$BIN/mxmlc -static-link-runtime-shared-libraries -output=../WebSocketMainInsecureNoTLS.swf WebSocketMainInsecureNoTLS.as &&
 $BIN/mxmlc -static-link-runtime-shared-libraries -output=../WebSocketMainNoTLS.swf WebSocketMainNoTLS.as &&
 cd .. &&
 zip WebSocketMainInsecure.zip WebSocketMainInsecure.swf &&
